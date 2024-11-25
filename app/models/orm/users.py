9ec46d3770f731f376a71961f199234b670b227e
lscope <l.scope98@gmail.com>
2024-11-25 06:54:19 +0000

    [fix] id default

--- app/models/orm/users.py
+++ app/models/orm/users.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from uuid import UUID
+from uuid import UUID, uuid4
 from sqlmodel import SQLModel, Field
 from datetime import datetime
 from pydantic import EmailStr
@@ -10,10 +10,13 @@ from . import USERS_TABLENAME
 class User(SQLModel, table=True):
     __tablename__ = USERS_TABLENAME
 
-    id: Optional[UUID] = Field(primary_key=True, description="User ID")
+    id: Optional[UUID] = Field(primary_key=True, default_factory=uuid4, description="User ID")
     email: EmailStr = Field(nullable=False, unique=True, description="User's email")
     username: str = Field(nullable=False, unique=True, description="User's username")
     password: str = Field(nullable=False, description="User's password")
     creation_dt: datetime = Field(nullable=False, default=datetime.now(), description="User creation date")
     verified: bool = Field(nullable=False, default=False, description="User's email verified")
     pw_attempts: int = Field(nullable=False, default=0, description="User tentatives to access with wrong credentials")
+
+    class Config:
+        from_attributes: bool = True
